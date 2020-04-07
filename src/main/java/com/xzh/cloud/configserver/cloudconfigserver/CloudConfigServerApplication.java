355ff2a1305c7818502edde8cebee8c4e2ee7195
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-04-07 22:39:51 +0800

    registry to eureka

--- src/main/java/com/xzh/cloud/configserver/cloudconfigserver/CloudConfigServerApplication.java
+++ src/main/java/com/xzh/cloud/configserver/cloudconfigserver/CloudConfigServerApplication.java
@@ -2,8 +2,17 @@ package com.xzh.cloud.configserver.cloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+//@EnableDiscoveryClient
+//@SpringCloudApplication
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {
